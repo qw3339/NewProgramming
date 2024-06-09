90f875c4f7c280ca8c278998e4740bbfe2aedf48
Анастасия <neverme039@gmail.com>
2024-06-09 17:50:16 +0400

    Ip

--- Ip/Ip/main.cpp
+++ Ip/Ip/main.cpp
@@ -17,6 +17,30 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 
 }
+BOOL checMask(DWORD mask)
+{
+	//DWORD  pattern = 1 << 32;
+	for (int i = 0; i < 32; i++)
+	{
+
+	}
+	return TRUE;
+}
+INT CountOnes(DWORD mask)
+{
+	INT zero_bits = 0;
+	DWORD power;
+	for (int i = 1; i ; i<<=1,zero_bits++)
+	{
+		if (mask & i)
+		{
+			power = i;
+			break;
+			
+		}
+	}
+	return 32-zero_bits;
+}
 
 //Процедура окна - это самая обычная функция, которая вызываеться при запуске окна
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
@@ -34,11 +58,14 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_INITDIALOG:
 	{
 		HWND nUpDown = GetDlgItem(hwnd, IDC_SPIN_PREFIC); 
-
 		//Функция GetDlgItem()  получает HWND элеменат интерфэйса по его ID
 		SendMessage(nUpDown, UDM_SETRANGE32, 0, 32);
+
+		HWND hIPaddares = GetDlgItem(hwnd, IDC_IPADDRESS);
+		SetFocus(hIPaddares);
 	}
 	break;
+
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
@@ -47,8 +74,11 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			CONST INT SIZE = 256;
 			CHAR sz_info[SIZE]{};
 
-			HWND hIPaddres = GetDlgItem(hwnd, IDC_IPADDRESS);
+			HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
 			HWND hStaticInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
+			HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+
+
 			//EN_ - Edit notification (Уведомление)  
 			if (HIWORD(wParam) == EN_CHANGE)
 			{
@@ -59,7 +89,7 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				//TBYTE - Ten Bytes десять байт = 80 bit;  
 				//DQWORD - Double Quad Word двойное учетверенное слово = 128 bit
 				DWORD dw_address;
-				SendMessage(hIPaddres, IPM_GETADDRESS, 0, (LPARAM)&dw_address);
+				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dw_address);
 				INT first = FIRST_IPADDRESS(dw_address);
 				INT second = SECOND_IPADDRESS(dw_address);
 				INT third =  THIRD_IPADDRESS(dw_address);
@@ -68,23 +98,100 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 				sprintf_s(sz_info, SIZE, "Info:\nFirst: %i, Second: %i, Third: %i, Forth: %i", first, second, third,  fourth);
 				SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)sz_info);
 
+				/////////////////////////////////////////////////////////////////
+				if (first < 128)SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"8");
+				else if(first< 192)SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"16");
+				else if (first < 224)SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)"24");
+
+
 
 			}
 		}			
 
 		break;
-		case IDOK: MessageBox(hwnd,"Была нажата кнопка OK","Info", MB_OK | MB_ICONINFORMATION); break;
+		case IDC_IPMASK: 
+		{
+			//if(HIWORD(wParam) == EN_CHANGE)
+			//{ 
+			//	HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+			//	HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+			//	DWORD dw_mask = 0;
+			//	SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dw_mask);
+			//	INT prefix = CountOnes(dw_mask);
+			//	CHAR sz_prefix[3] = {}; //sz_ - string zero(строкаб заканчивающаяся нулем)
+			//	sprintf(sz_prefix, "%i", prefix);
+
+			//	SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)sz_prefix);
+			//	//SendMessage(hIPmask, IPM_SETADDRESS, 0, dw_mask >> (32 - prefix) << (32 - prefix));
+			//}
+			
+		}
+		break;
+
+		case IDC_EDIT_PREFIX:
+		{
+			HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+			HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+			//DWORD dw_mask = UINT_MAX;
+			DWORD dw_mask = ~0;
+			if (HIWORD(wParam) == EN_CHANGE)
+			{
+				CONST INT SIZE = 8;
+				CHAR sz_buffer[SIZE];
+				SendMessage(hEditPrefix, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				//MessageBox(hwnd, sz_buffer, "Prefix", MB_OK | MB_ICONINFORMATION);
+				INT shift = atoi(sz_buffer);
+
+				//dw_mask >>= (32 - shift);//Функция atoi() преобразует ASCII-строку в занчение типа int
+				//dw_mask <<= (32 - shift);
+				//SendMessage(hIPmask, IPM_SETADDRESS, 0, dw_mask);
+				SendMessage(hIPmask, IPM_SETADDRESS, 0, dw_mask>> (32 - shift) << (32 - shift));
+			}
+		}
+		break;
+
+		case IDOK:  MessageBox(hwnd,"Была нажата кнопка OK","Info", MB_OK | MB_ICONINFORMATION);  
+			break;
 			//MB_OK | MB_ICONINFORMATION - | это or
 			// || - логическое OR
 			// | - побитовое OR
 		case IDCANCEL: EndDialog(hwnd, 0); break;
 		}
 		break;
+
+	case WM_NOTIFY:
+	{
+		HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+		HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+		switch (LOWORD(wParam))
+		{
+			case IDC_IPMASK:
+			{
+
+				DWORD dw_mask = 0;
+				SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dw_mask);
+				int i = 32;
+				for (; dw_mask & 1 ^ 1; i--)dw_mask >>= 1;
+				CHAR sz_prefix[5]{};
+				sprintf(sz_prefix, "%i", i);
+
+				SendMessage(hEditPrefix, WM_SETTEXT, 0, (LPARAM)sz_prefix);
+
+			}
+			break;
+			
+		}
+		break;
+
+	}
+	break;
 	case WM_CLOSE:
 
 		EndDialog(hwnd, 0);
+		break;
 
 	}
+
 	return FALSE;
 
 } 
