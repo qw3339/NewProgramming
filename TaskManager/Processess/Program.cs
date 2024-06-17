da51270830fcd9fe053213e02378627d16b59105
Анастасия <neverme039@gmail.com>
2024-06-17 12:58:19 +0400

    TaskManager

--- TaskManager/Processess/Program.cs
+++ TaskManager/Processess/Program.cs
@@ -1,4 +1,5 @@
-﻿//#define SINGLE_PROCESS
+﻿#define SINGLE_PROCESS
+//#define ALL_PROCESS
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,26 +38,62 @@ namespace Processess
 
 			Console.WriteLine($"SessionID: {process.SessionId}");
 			Console.WriteLine($"Threads: {process.Threads}");
-			Console.WriteLine($"Threads: {process.PriorityClass}"); 
-#endif
+			Console.WriteLine($"Threads: {process.PriorityClass}");
 			
+			PerformanceCounter counter = new PerformanceCounter("Process","% Processor Time", process.ProcessName, true);
+			Console.WriteLine("Press any key to continue...");
+			while(!Console.KeyAvailable)
+			{
+				Console.Clear();
+				double proccent = counter.NextValue();
+				Console.WriteLine($"{process.ProcessName} CPU load: {proccent/10} %");
+				int units = 1024 * 1024;
+				Console.WriteLine($"Working Set:		{process.WorkingSet64} ");
+				Console.WriteLine($"Private working Set:{process.PrivateMemorySize64/units} ");
+				
+				System.Threading.Thread.Sleep(100);
+
+
+			}
+			process.CloseMainWindow();
+			process.Dispose();
+			process = null;
+
+
+
+
+
+#endif
+
+#if ALL_PROCESS
 				Process[] processes = System.Diagnostics.Process.GetProcesses();
 				for(int i = 0; i < processes.Length; i++)
 				{
-					//Console.WriteLine($"{processes[i].Id}\t{processes[i].MainModule.FileName}");
+					Console.WriteLine($"{processes[i].Id}\t{processes[i].MainModule.FileName}");
 					//Console.Write($"Name: {processes[i].ProcessName}\t");
 					//Console.Write($"PID: {processes[i].Id}\t");
 					//Console.Write($"Path: {processes[i].MainModule.FileName}\t");
 					//Console.WriteLine();
 
-			}
+				}
+
+		
+#endif
+
+
 
 		}
+
+
 		[DllImport("advapi32.dll", SetLastError = true)]
 		private static extern bool OpenProcessToken(IntPtr processHandle, uint desiredAcsess, out IntPtr handle);
 		[DllImport("kernel32.dll",SetLastError = true)]
 		[return: MarshalAs(UnmanagedType.Bool)]
 		private static extern bool CloseHandle(IntPtr handle);
+
+
+
+
 
 
 	}
--- TaskManager/TaskManager/Form1.cs
+++ TaskManager/TaskManager/Form1.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+
 
 namespace TaskManager
 {
@@ -15,6 +18,44 @@ namespace TaskManager
         public MainForm()
         {
             InitializeComponent();
+			SetColumns();
+            statusStrip1.Items.Add("");
+            listViewProcesses.SetS
+
+
+
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+            LoadProcesses();
+
+		}
+        void SetColumns()
+        {
+
+            listViewProcesses.Columns.Add("PID");
+            listViewProcesses.Columns.Add("Name");
+
+
+		}
+
+        void LoadProcesses()
+        {
+            listViewProcesses.Items.Clear();
+
+            Process[] processes = Process.GetProcesses();
+            for (int i = 0; i < processes.Length; i++)
+            {
+                ListViewItem item = new ListViewItem();
+                item.Text = processes[i].Id.ToString();
+                item.SubItems.Add(processes[i].ProcessName);
+                listViewProcesses.Items.Add(item);
+
+
+
+			}
+            statusStrip1.Items[0].Text = ($"Количество процессов: {listViewProcesses.Items.Count}");
         }
-    }
+	}
 }
